d116243087ec383ac8349954b3f76b0520067669
palak jaiswal <108883565+palakjaiswal108@users.noreply.github.com>
2023-04-23 17:31:25 +0530

    Create experiment02.py

---
+++ experiment02.py
@@ -0,0 +1,14 @@
+# palak jaiswal (0827CI201126) 
+#List Comprehension 
+ number_list = [ x for x in range(20) if x % 2 == 0] print(number_list) 
+# palak jaiswal (0827CI201126) 
+#List Comprehension 
+
+lettersList = list(map(lambda x: x, 'human')) print(lettersList) 
+# palak jaiswal (0827CI201126) 
+#List Comprehension 
+
+num_list = [y for y in range(100) if y % 2 == 0 if y % 5 == 0] print(num_list) 
+# palak jaiswal (0827CI201126) 
+#List Comprehension 
+ obj = ["Even" if i%2==0 else "Odd" for i in range(10)] print(obj)  
